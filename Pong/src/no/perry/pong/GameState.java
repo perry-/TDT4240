620d0cb17473262b9c512928682d060a34555380
Alexander Perry <yrrepxela@gmail.com>
2012-02-12 20:02:09 +0100

    Added game over state

--- Pong/src/no/perry/pong/GameState.java
+++ Pong/src/no/perry/pong/GameState.java
@@ -10,9 +10,9 @@ import sheep.graphics.Font;
 import sheep.graphics.Color;
 import sheep.graphics.Image;
 import sheep.input.TouchListener;
-import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
 import android.graphics.Typeface;
 import android.util.Log;
@@ -22,31 +22,30 @@ import android.view.MotionEvent;
 
 /**
  * @author Perry
- * Git test
  */
 public class GameState extends State implements TouchListener, CollisionListener {
 
-	private int canvasHeight, canvasWidth, pl1Score, pl2Score;
+	private int canvasHeight, canvasWidth, pl1Score, pl2Score, winner = 0;
 	private Sprite paddle1, paddle2;
 	private Ball ball;
 	private Paint paint;
 	private Canvas can;
+	private Display display;
 	private CollisionLayer collisionLayer = new CollisionLayer();
 	private World world = new World();
 	private Image paddle1Image = new Image(R.drawable.paddle1);
 	private Image paddle2Image = new Image(R.drawable.paddle2);
 	private Image ballImage = new Image(R.drawable.ball);
-	private Display display;
 
 
 	
 	/**
 	 * Constructor, add paddles and a ball to the canvas
-	 * @param display Display data, for better scaling
+	 * @param display Display data, for adjusting the objects before anything is drawn
 	 */
-	public GameState(Display display, Resources res) {
+	public GameState(Display display) {
 		this.display = display;
-
+		
 		paddle1 = new Sprite(paddle1Image);
 		paddle2 = new Sprite(paddle2Image);
 		ball = Ball.instance(ballImage);
@@ -55,10 +54,11 @@ public class GameState extends State implements TouchListener, CollisionListener
 		collisionLayer.addSprite(paddle2);
 		collisionLayer.addSprite(ball);
 
+		
+		pl1Score = 20;
+		pl2Score = 20;
 		paddle1.setPosition(display.getHeight()/2, display.getHeight()/4);
 		paddle2.setPosition(display.getHeight()/2, display.getHeight()-(display.getHeight()/4));
-		pl1Score = 0;
-		pl2Score = 0;
 
 		ball.setPosition(display.getWidth()/2, display.getHeight()/2);
 		ball.setSpeed(100, 100);
@@ -87,9 +87,11 @@ public class GameState extends State implements TouchListener, CollisionListener
 
 		Font font1 = new Font(0, 255, 0, 30, Typeface.SERIF, Typeface.NORMAL);
 		Font font2 = new Font(255, 0, 0, 30, Typeface.SERIF, Typeface.NORMAL);
+		font1.setTextAlign(Align.CENTER);
+		font2.setTextAlign(Align.CENTER);
 
-		can.drawText(pl1Score==21 ? "Player 1 has won!" : "Score player 1: "+pl1Score, (canvasWidth/2)-100, (canvasHeight/2)-20, font1);
-		can.drawText(pl2Score==21 ? "Player 2 has won!" : "Score player 2: "+pl2Score, (canvasWidth/2)-100, (canvasHeight/2)+20, font2);
+		can.drawText("Score player 1: "+pl1Score, canvasWidth/2, canvasHeight/2-20, font1);
+		can.drawText("Score player 2: "+pl2Score, canvasWidth/2, canvasHeight/2+20, font2);
 
 		ball.draw(can);
 		world.draw(can);
@@ -115,6 +117,11 @@ public class GameState extends State implements TouchListener, CollisionListener
 					ball.setYSpeed(-ball.getSpeed().getY());
 					pl2Score++; 
 				}
+			}else{
+				if(pl1Score == 21){ winner = 2; }
+				if(pl2Score == 21){ winner = 1; }
+				getGame().popState();
+				getGame().pushState(new GameOverState(winner, display));
 			}
 
 			if(paddle1.getX()>=0 && paddle1.getX()<=canvasWidth){
@@ -134,12 +141,13 @@ public class GameState extends State implements TouchListener, CollisionListener
 	}
 
 	public boolean onTouchMove(MotionEvent event) {
-		if(event.getX()<(display.getWidth()-(paddle2Image.getWidth()/2)) && event.getX()>(paddle2Image.getWidth()/2)){
-			if(event.getY()>(display.getHeight()-(display.getHeight()/4)) && event.getY()<(display.getHeight())){
-				paddle2.setPosition(event.getX(), paddle2.getPosition().getY());
-				return true;
+			if(event.getX()<(display.getWidth()-(paddle2Image.getWidth()/2)) && event.getX()>(paddle2Image.getWidth()/2)){
+				if(event.getY()>(display.getHeight()-(display.getHeight()/4)) && event.getY()<(display.getHeight())){
+					paddle2.setPosition(event.getX(), paddle2.getPosition().getY());
+					return true;
+				}
 			}
-		}
+
 		return false;
 	}
 
